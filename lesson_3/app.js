db10aa9c5061da8a8d79c636257637419a2abbcb
DoanDangManhTuan <Doanmanhtuan596@gmail.com>
2024-01-07 15:05:16 +0700

    buoi_3

---
+++ lesson_3/app.js
@@ -0,0 +1,45 @@
+// var computer = {
+//     brand: 'asus',
+//     status: '5 years old',
+//     adviced: 'need to be renew',
+// }
+// var person = {
+//     gender: 'male',
+//     age: 18,
+//     name: 'john',
+// }
+// var tools = {
+//     type: 'hammer',
+//     usage: 'to smash things',
+//     caution: 'maybe dangerous',
+// }
+// var gardening_tools = {
+//     type: 'water bucket',
+//     usage: 'to water your d.ead plants',
+//     note: 'can be heavy',
+// }
+// var school_supplies = {
+//     type: 'pencil',
+//     usage: 'to write things',
+//     note: 'can be dangerous',
+// }
+// var furniture = {
+//     type: 'bed',
+//     usage: 'to sleep and rest',
+// }
+// for (var key in person) {
+//     console.log(key+":"+person[key])
+// }
+// person.age = 20
+// for (var key in person) {
+//     console.log(key+":"+person[key])           
+// }
+// var fruits = ["apple","pineapple","orange"]
+
+// for (var fruit of fruits){console.log(fruit)}
+// for (let i = 0 ; i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+// fruits.forEach(function(fruit){
+//     console.log(fruit)
+// })
